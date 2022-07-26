71c6fbf11ade24e18429391faa5efcc197033126
AdamEth98 <adam.etherington1@outlook.com>
2022-07-26 10:51:40 +0100

    capitalise topic slug when displayed

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function TopicCard({topic}) {
   return (
     <article className="topic-card-con">
-      <h2>{topic.slug}</h2>
+      <h2>{topic.slug ? topic.slug[0].toUpperCase() + topic.slug.substring(1) : ""}</h2>
       <p>{topic.description}</p>
       <Link to={`/topics/${topic.slug}`}>{'View Articles ->'}</Link>
     </article>
